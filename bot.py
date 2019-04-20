ef6ad28691cec211300f87098978b1ad06ac72ce
ILyas08 <IL.945@mail.ru>
2019-04-20 21:00:19 +0300

    heroku

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
 import telebot
 
-bot = telebot.Telebot('')
+bot = telebot.TeleBot('860528282:AAGR6kaJ_K4RYlzJo8fXgZsDdrcFAW9N-lw')
+
+@bot.message_handler(func=lambda message: True)
+def send_welcome(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
